ae422098bb05fb1d21bd754674ec9f0816b7031e
Nicholas Villacreces <nvillacreces72@gmail.com>
2020-06-06 16:05:51 -0500

    search

---
+++ src/components/SearchResultElement.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default class SearchResultElement extends Component {
+    render() {
+        return (
+            <div class="search-result-element">
+                <div class="search-result-title">
+
+                </div>
+                <div className="search-result-author">
+
+                </div>
+                <div className="search-result-isbn">
+
+                </div>
+                <div className="serach-result-desc">
+                    
+                </div>
+            </div>
+        )
+    }
+}
---
+++ src/components/SearchResultList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default class SearchResultList extends Component {
+
+    constructor(props){
+        super(props)
+        this.state ={}
+    }
+
+
+    render() {
+        return (
+            <div className="search-result-list">
+                
+            </div>
+        )
+    }
+}
