fb97cefe1c909f6136c9e8e1a5ca6d9bf4133e59
Nicholas Villacreces <nvillacreces72@gmail.com>
2020-06-10 16:40:04 -0500

    searchResults Listed

--- src/components/SearchResultElement.js
+++ src/components/SearchResultElement.js
@@ -17,17 +17,15 @@ export default class SearchResultElement extends React.Component {
         return (
             <div className="search-result-element">
                 <div className="search-result-title">
-                   
+                   {this.props.volumeInfo.title}
                 </div>
                 <div className="search-result-author">
-                    
+                    {this.props.volumeInfo.authors}
                 </div>
                 <div className="search-result-isbn">
-                    
+                    {this.props.industryIndentifiers}
                 </div>
-                <div className="serach-result-desc">
-                    
-                </div>
+                
             </div>
         )
     }
